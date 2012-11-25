32881f93054bdeeee1750421142dc47d0d4b11ba
Yazen Ghannam <yghannam@gmail.com>
2012-11-25 10:26:57 -0500

    Implemented S-expressions using PyParsing

--- snc.py
+++ snc.py
@@ -6,6 +6,7 @@ Semantic Network Creater
 """
 
 import sys, re
+from pyparsing import *
 
 if __name__ == '__main__':
     print 'Hello'
@@ -28,7 +29,38 @@ if __name__ == '__main__':
         elif parsing:
             sentences[i] += line.strip("\t\n ")
             
-    print list(sentences[0])
+    """        
+    fileOut = open("famine.out", 'w')
+    for x in sentences:
+        fileOut.write(x)
+        fileOut.write("\n\n")
+    fileOut.close()    
+    #print sentences[0]
+    """
+    
+    
+    # define grammar
+    # define punctuation literals
+    LPAR, RPAR, LBRK, RBRK, LBRC, RBRC, VBAR = map(Suppress, "()[]{}|")
+    
+    sexp = Forward()
+    sexpList = Group(LPAR + ZeroOrMore(sexp) + RPAR)
+    sexp << ( Word(alphanums+"-_*/") | sexpList )
+
+    """
+    gElement = "(" + Word( alphanums ) + ")"
+    gPhrase = "(" + Word( alphanums ) + ZeroOrMore( gPhrase ) + ")"
+    gDT = "(DT" + Word(alphanums) + ")"
+    gADJ = "(ADJ" + Word(alphanums) + ")"
+    gNOUN = "(NOUN" + Word(alphanums) + ")"
+    gNP = "(" + ZeroOrMore(gDT) + ZeroOrMore(gADJ) + ZeroOrMore(gNOUN) + ")"
+    gSemanticRole = "(" + "THEME" + ")"
+    gSense = "(" + OneOrMore(gElement) + ")"
+    #gPrep = "(PREP" + 
+    gSubj = "(SUBJ" + gNP + gSense + gSemanticRole + gPrep + ")"
+    gSentence = "(" + Word(alphanums) + OneOrMore( gElement ) +")"
+    """
+    print sexp.parseString( sentences[0] )
     
     #contents = file.read()
     #print len(re.findall('famine', contents))
